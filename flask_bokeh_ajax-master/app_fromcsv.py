7956e1b944d82ea90c207fba5f5d5ee091862459
Erika Lu <erika.lu@students.olin.edu>
2018-05-31 10:53:45 -0400

    Working variable buttons!

--- flask_bokeh_ajax-master/app_fromcsv.py
+++ flask_bokeh_ajax-master/app_fromcsv.py
@@ -24,66 +24,106 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def home():
+    #Open file and create sources
     dictionary = open_file_into_dictionary('SampleCSV2.csv')
     keys = list(key.title() for key in dictionary.keys())
     values = [value for value in dictionary.values()]
-    source = ColumnDataSource(data=dict(x=values[0], y=values[1]))
-    source2 = ColumnDataSource(data = dict(keys = keys, values = values))
-    print(values)
+    xy_source = ColumnDataSource(data=dict(xs=[values[0]], ys=[values[1]], labels = [keys[1]],
+    colors = ['red', 'green', 'blue', 'purple', 'brown', 'aqua']))
+    variables_source = ColumnDataSource(data = dict(keys = keys, values = values))
 
 
 
 
+    #Create general plot
     plot = figure(plot_width=800, plot_height=600, toolbar_location = 'left')
     plot.title.text_font= 'helvetica'
     plot.title.text_font_size = '24pt'
     plot.title.align = 'center'
     plot.title.text_font_style = 'normal'
+    plot.multi_line(xs = 'xs', ys = 'ys', legend = 'labels', line_color = 'colors', source = xy_source)
 
-    plot.line('x', 'y', source = source, line_width=3, line_alpha=0.6)
-    plot.multi_line(xs = [[10,20, 30],[10, 20, 30]], ys = [[12,30,40],[1, 20,30]])
 
-    x_axis_callback = CustomJS(args=dict(source=source, source2 = source2, axis = plot.xaxis[0]), code="""
-        var data = source.data;
-        var data2 = source2.data;
+    #Define callbacks
+    x_axis_callback = CustomJS(args=dict(xy_source = xy_source, variables_source = variables_source,
+     axis = plot.xaxis[0]), code="""
+        var xy_data = xy_source.data;
+        var variables_data = variables_source.data;
         var index = cb_obj.active;
-        var keys = data2.keys;
+        var values = variables_data.values;
+
+        var y_length = xy_data['ys'].length;
+        var new_list = [];
+        for (i = 0; i < y_length; i++) {
+        new_list.push(values[index])}
+        xy_data['xs'] = new_list;
+
+        xy_source.change.emit();
+
+        var keys = variables_data.keys;
         var label = keys[index];
         axis.axis_label = label;
-        var values = data2.values;
-        var new_list = values[index];
-        var x = data['x'];
-        var y = data['y'];
-        for (var i = 0; i < x.length; i++) {
-                x[i] = new_list[i];
-            }
-        source.change.emit();
     """)
 
+    y_axis_callback = CustomJS(args=dict(xy_source = xy_source, variables_source = variables_source,
+     axis = plot.yaxis[0]), code="""
+
+        var xy_data = xy_source.data;
+        var variables_data = variables_source.data;
+        var index_list = cb_obj.active;
+
+        var values = variables_data.values;
+        var index_length = index_list.length;
+        var keys = variables_data.keys;
 
 
+
+
+        var new_ys = [];
+        var new_labels = [];
+        for (i = 0; i < index_length; i++) {
+            new_ys.push(values[index_list[i]]);
+            new_labels.push(keys[index_list[i]])}
+        xy_data['labels'] = new_labels;
+        xy_data['ys'] = new_ys;
+
+        var x_variable = xy_data['xs'][0];
+        var new_x = [];
+        for (i = 0; i < index_length; i++) {
+            new_x.push(x_variable)}
+        xy_data['xs'] = new_x;
+
+        xy_source.change.emit();
+
+
+
+    """)
+
+#
+# xy_source.change.emit();
+#
+# var keys = variables_data.keys;
+# var label = keys[index_list[0]];
+# axis.axis_label = label;
+    #Create toolbox
     label_x = Div(text="""X-Axis""", width=200)
     x_axis = RadioButtonGroup(labels=keys, active=0, callback = x_axis_callback)
     label_y = Div(text="""Y-Axis""", width=200)
-    y_axis = CheckboxButtonGroup(labels=keys, active=[1])
+    y_axis = CheckboxButtonGroup(labels=keys, active=[1], callback = y_axis_callback)
     label_axes = Div(text="""<br />Modify Labels""", width=200)
     title_name = TextInput(title="Title", value='Default Title')
     plot.title.text = title_name.value
-    x_name = TextInput(title="X-Axis", value=keys[0])
-    plot.xaxis.axis_label = x_name.value
-    y_name = TextInput(title="Y-Axis", value=keys[1])
-    plot.yaxis.axis_label = y_name.value
-
-
-
-
+    x_name = TextInput(title="X-Axis", value='Default X Label')
+    plot.xaxis.axis_label = keys[0]
+    y_name = TextInput(title="Y-Axis", value='Default Y Label')
+    plot.yaxis.axis_label = keys[1]
 
     toolbox = widgetbox(label_x, x_axis, label_y, y_axis, label_axes, title_name, x_name, y_name)
 
 
 
 
-
+    #Integrate with html
     parts = dict(toolbox = toolbox, plot = plot)
     script, div = components(parts, INLINE)
     return render_template('plotpage.html',
